fa32c5135a2c1b74fba73e6efb803fbf40ca9308
salihkinali <salih_3220@hotmail.com>
2023-03-04 21:26:46 +0300

    add features and navigation module

---
+++ core/design/src/main/java/com/salihkinali/core/design/base/BaseAdapter.kt
@@ -0,0 +1,28 @@
+package com.kekodstudy.core.ui.base
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class BaseAdapter<T : Any, VH : BaseViewHolder<T>> :
+    RecyclerView.Adapter<VH>() {
+
+    private val items = mutableListOf<T>()
+
+    fun updateItems(newItems: List<T>) {
+        items.apply {
+            clear()
+            addAll(newItems)
+            notifyDataSetChanged()
+        }
+    }
+
+    fun getItem(position: Int) = items[position]
+
+    abstract override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.onBind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+}
--- feature/home/src/main/java/com/salihkinali/feature/home/HomeFragment.kt
+++ feature/home/src/main/java/com/salihkinali/feature/home/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.salihkinali.storeease
+package com.salihkinali.feature.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
