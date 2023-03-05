2e15ee4a286ae15db8e2743ae04c4a914fbfcac2
salihkinali <salih_3220@hotmail.com>
2023-03-05 12:50:47 +0300

    fixed circle dependency problem in data module

--- feature/home/src/main/java/com/salihkinali/feature/home/HomeFragment.kt
+++ feature/home/src/main/java/com/salihkinali/feature/home/HomeFragment.kt
@@ -3,9 +3,12 @@ package com.salihkinali.feature.home
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.salihkinali.core.common.NetworkResponse
 import com.salihkinali.core.design.base.BaseFragment
 import com.salihkinali.feature.home.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -14,6 +17,18 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.generalText.text = viewModel.homeUidata.toString()
+        observeProductData()
+    }
+
+    private fun observeProductData() {
+        lifecycleScope.launch {
+            viewModel.homeUidata.collect {
+                when (it) {
+                    is NetworkResponse.Error -> binding.generalText.text = it.exception.toString()
+                    NetworkResponse.Loading -> Unit
+                    is NetworkResponse.Success -> binding.generalText.text = it.result.toString()
+                }
+            }
+        }
     }
 }
