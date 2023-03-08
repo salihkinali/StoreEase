5c7504c3686632b6be0754326e2fdf58b4dcd2e4
salihkinali <salih_3220@hotmail.com>
2023-03-08 23:55:19 +0300

    it changed detail xml

--- app/src/main/java/com/salihkinali/storeease/MainActivity.kt
+++ app/src/main/java/com/salihkinali/storeease/MainActivity.kt
@@ -2,12 +2,39 @@ package com.salihkinali.storeease
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.salihkinali.core.design.extension.hide
+import com.salihkinali.core.design.extension.show
+import com.salihkinali.storeease.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.detailFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
--- feature/detail/src/main/java/com/salihkinali/feature/detail/DetailFragment.kt
+++ feature/detail/src/main/java/com/salihkinali/feature/detail/DetailFragment.kt
@@ -1,15 +1,24 @@
 package com.salihkinali.feature.detail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import com.salihkinali.core.common.NetworkResponse
 import com.salihkinali.core.design.base.BaseFragment
+import com.salihkinali.core.design.extension.downloadImage
+import com.salihkinali.core.design.extension.hide
+import com.salihkinali.core.design.extension.show
 import com.salihkinali.feature.product.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
-class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate){
+class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate) {
 
     private val viewModel by viewModels<DetailViewModel>()
     private val navArgs by navArgs<DetailFragmentArgs>()
@@ -17,6 +26,32 @@ class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getDetailProduct(navArgs.productId)
+        showDetailData()
+    }
 
+    @SuppressLint("SetTextI18n")
+    private fun showDetailData() {
+        binding.apply {
+            lifecycleScope.launch {
+                viewModel.detailUiData.collectLatest {
+                    when (it) {
+                        is NetworkResponse.Error -> Unit
+                        NetworkResponse.Loading -> binding.progressBar.show()
+                        is NetworkResponse.Success -> {
+                            binding.apply {
+                                progressBar.hide()
+                                productImageView.downloadImage(it.result.image)
+                                productTitleText.text = it.result.title
+                                productDescriptionText.text = it.result.description
+                                ratingBar.rating = it.result.rate.toFloat()
+                                stockCount.text = "Stock Count: ${it.result.count}"
+                                productPieceText.text = "1"
+                                productPriceText.text = it.result.price.toString()
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
