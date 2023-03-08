9ce5e2d98383bc84e9e1803c2ed5ac6f0afdae6f
salihkinali <salih_3220@hotmail.com>
2023-03-08 21:58:46 +0300

    product detail observed in detailFragment

--- feature/detail/src/main/java/com/salihkinali/feature/detail/DetailFragment.kt
+++ feature/detail/src/main/java/com/salihkinali/feature/detail/DetailFragment.kt
@@ -2,6 +2,8 @@ package com.salihkinali.feature.detail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.salihkinali.core.design.base.BaseFragment
 import com.salihkinali.feature.product.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,7 +11,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DetailFragment : BaseFragment<FragmentDetailBinding>(FragmentDetailBinding::inflate){
 
+    private val viewModel by viewModels<DetailViewModel>()
+    private val navArgs by navArgs<DetailFragmentArgs>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel.getDetailProduct(navArgs.productId)
+
     }
 }
--- feature/detail/src/main/java/com/salihkinali/feature/detail/DetailViewModel.kt
+++ feature/detail/src/main/java/com/salihkinali/feature/detail/DetailViewModel.kt
@@ -1,10 +1,43 @@
 package com.salihkinali.feature.detail
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.salihkinali.core.common.NetworkResponse
+import com.salihkinali.core.domain.entity.ProductDetailEntity
+import com.salihkinali.core.domain.usecase.GetDetailProductUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailViewModel @Inject constructor() : ViewModel(){
+class DetailViewModel @Inject constructor(
+    private val getDetailProductUseCase: GetDetailProductUseCase
+) : ViewModel() {
 
+    private val _detailUiData = MutableStateFlow<NetworkResponse<ProductDetailEntity>>(
+        NetworkResponse.Loading
+    )
+    val detailUiData = _detailUiData.asStateFlow()
+
+     fun getDetailProduct(productId: Int) {
+        viewModelScope.launch {
+            getDetailProductUseCase.invoke(productId).collect { productDetail ->
+                when (productDetail) {
+                    is NetworkResponse.Error -> {
+                        _detailUiData.emit(NetworkResponse.Error(productDetail.exception))
+                    }
+                    NetworkResponse.Loading -> {
+                        _detailUiData.emit(NetworkResponse.Loading)
+                    }
+                    is NetworkResponse.Success -> {
+                        Log.i("GELEN DATA BURDA ->", productDetail.result.description)
+                        _detailUiData.emit(NetworkResponse.Success(productDetail.result))
+                    }
+                }
+            }
+        }
+    }
 }
